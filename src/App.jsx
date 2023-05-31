7aaa78e8e2e01134521451d5f3fdbc70948ff689
adrian-ext <adrian-ext@payflows.io>
2023-05-31 10:57:10 +0300

    updated app solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import CardComponent from "./CardComponent.jsx";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState("");
+
   return (
     <div>
-      <CardComponent
-        content={
-          "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam."
-        }
-      ></CardComponent>
+      <input
+        onChange={(ev) => {
+          let text = ev.target.value;
+          setInputText(text);
+        }}
+        type="text"
+      />
+      <button
+        onClick={() => {
+          let itemsNew = [...items, inputText];
+          setItems(itemsNew);
+        }}
+      >
+        Add Item
+      </button>
+      {items.map((item) => (
+        <CardComponent content={item}></CardComponent>
+      ))}
     </div>
   );
 }
