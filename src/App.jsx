d6754d57457d23c3596984acdd4bb1cf5cac30a6
metehandoenmez <metehandoenmez@gmail.com>
2023-06-14 01:04:08 +0300

    updated exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardComponent from "./CardComponent.jsx";
 
 function App() {
-  
+
+  const [items, setItems] = useState([]);
   const [inputText, setInputText] = useState("")
-  const [items, setItems] = useState([])
-  
-  
-  return (
-    <div>
-      <input onChange={(ev) => {
-      let text = ev.target.value;
-      setInputText(text);
-    }
-      } type="text" />
-      <button onClick={() => {
-      let itemsArr = [...items, inputText]
-      setItems(itemsArr) }
-      }>Add Text</button>
-      {items.map((el) => <CardComponent content={el}>
-    </CardComponent>) }
-    </div>
-  );
+
+useEffect(() => {
+  console.log(items)
+},[items])
+useEffect(() => {
+  console.log(inputText)
+},[inputText])
+
+
+let handleInputChange = (event) => {
+  setInputText(event.target.value)
 }
 
+
+
+  return (
+    <div>
+      <input type="text" onChange={handleInputChange}/>
+      <button onClick={() => {
+        let updateArray = [...items, inputText];
+        setItems(updateArray)
+      }}>Add item</button>
+      <br />
+      {items.map((item, index) => {
+        return <CardComponent content={item}/>
+      })
+    }
+    </div>
+
+  )
+}
 export default App;
