18c2bf68e2cda380669d8ac258f05c5f34c38f06
metehandoenmez <metehandoenmez@gmail.com>
2023-06-14 10:22:10 +0300

    updated exercise

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,48 @@ import CardComponent from "./CardComponent.jsx";
 function App() {
   const [items, setItems] = useState([]);
   const [inputText, setInputText] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState(null);
 
-  return (
-    <div>
+  const updateItem = (index, text) => {
+    let toUpdateItems = [...items];
+    toUpdateItems[index] = text;
+    setItems(toUpdateItems);
+  };
+
+  const deleteItem = (index) => {
+    // needs to delete the item in the items array
+    let toUpdateItems = [...items];
+    toUpdateItems.splice(index, 1);
+    setItems(toUpdateItems);
+  };
+
+  
+
+    if(!loggedIn) {
+      return (
+        <div>
+          <h1>Login Screen</h1>
+          Username :<input type="text" onChange={(event) => {
+            let text = event.target.value;
+            setInputText(text)
+          }} />
+          <button onClick={(event) => {
+            setUsername(event.target.value)
+            setLoggedIn(true)
+            setUsername(inputText)
+          }}>Login</button>
+        </div>
+      )
+    }
+      else if(loggedIn) {
+        return (
+          <div>
+            Welcome, {username}! <br />
+            <button onClick={() => {
+              setLoggedIn(false)
+            }}>Logout</button>
+            <br />
       <input
         onChange={(ev) => {
           let text = ev.target.value;
@@ -23,11 +62,18 @@ function App() {
       >
         Add Item
       </button>
-      {items.map((item) => (
-        <CardComponent content={item}></CardComponent>
+      {items.map((item, index) => (
+        <CardComponent
+          updateItem={updateItem}
+          deleteItem={deleteItem}
+          index={index}
+          content={item}
+        ></CardComponent>
       ))}
     </div>
-  );
+        )
+    }
+    
 }
 
 export default App;
