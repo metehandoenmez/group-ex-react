9dce80027c47ac71a89d18855874a4d6e45de570
metehandoenmez <metehandoenmez@gmail.com>
2023-06-19 14:13:14 +0300

    completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import {Routes, Route, Link} from "react-router-dom"
 
 import NumberDisplay from './NumberDisplay';
+import CardComponent from "./CardComponent";
+import NotFound from "./NotFound";
+import WelcomeComponent from "./WelcomeComponent";
+
+const style = {
+  border: "1px solid orange",
+  padding: "24px",
+  borderRadius: "16px"
+}
 
 function App() {
   return (
     <div>
-      <NumberDisplay></NumberDisplay>
+      <div style={style}>
+        <Routes>
+            <Route path="/" element={<WelcomeComponent></WelcomeComponent>}>
+              
+            </Route>
+            <Route path="/card" element={<CardComponent></CardComponent>}>
+    
+            </Route>
+            <Route path="/number" element={<NumberDisplay></NumberDisplay>}>
+              
+            </Route>
+            <Route path="*" element={<NotFound></NotFound>}>
+              
+            </Route>
+        </Routes>
+      </div>
+      <br />
+      <h3>Pages:</h3>
+      <Link to={"/"}>Home</Link>
+      <br />
+      <Link to={"/card"}>Card Component</Link>
+      <br />
+      <Link to={"/number"}>Number Display</Link>
     </div>
+
   );
 }
 
